48fcc3bdfa02e6e931c7c67dac0ca3d4222050fd
Karen <kareninejapiassu@gmail.com>
2021-03-06 01:03:42 +0100

    Fetch & Display Repos

--- API_Projects/GithubFinder/github.js
+++ API_Projects/GithubFinder/github.js
@@ -2,15 +2,21 @@ class GitHub {
   constructor() {
     this.client_id = 'e9b105a7d7d465d71df7';
     this.client_secret = 'bcb7cbf188beb18e715765c53d8f1acd59ab7829';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
