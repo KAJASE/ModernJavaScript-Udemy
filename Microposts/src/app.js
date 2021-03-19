6027c2fff88482437006c76fafd63d0539b4b36d
Karen <kareninejapiassu@gmail.com>
2021-03-19 01:27:06 +0100

    Create the UI

---
+++ Microposts/src/app.js
@@ -0,0 +1,17 @@
+/*const greeting = 'Hello World';
+console.log(greeting);
+
+const getData = async (url) => {
+  const response = await fetch(url);
+  const result = await response.json();
+  console.log(result);
+};
+
+getData('https://jsonplaceholder.typicode.com/posts');*/
+
+//CommonJS Module Sintax
+//const person = require('./mymodule');
+
+//ES2015 module
+//import { person } from './mymodule2';
+//console.log(person.name);
