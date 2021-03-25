08a10283b85cbf6e588c05bb92a1cb9763edceae
Karen <kareninejapiassu@gmail.com>
2021-03-25 19:35:53 +0100

    Removing Posts

--- Microposts/src/app.js
+++ Microposts/src/app.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', getPosts);
 //Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+//listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
 function getPosts(){
   http.get('http://localhost:3000/posts')
     .then(data => ui.showPost(data))
@@ -31,4 +34,20 @@ function submitPost() {
       getPosts();
     })
     .catch(err => console.log(err));
+}
+
+//DElete post
+function deletePost(e){
+  e.preventDefault();
+  if(e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if(confirm('Are you sure?')){
+      http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post removed', 'alert alert-success');
+          getPosts();
+        .catch(err => console.log(err));
+        });
+    }
+  }
 }
